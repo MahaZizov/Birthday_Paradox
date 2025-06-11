74cc0a8804aaaaf7f79e462a23284b6548786c51
maha <mahammadabdulazizov@outlook.com>
2025-06-12 00:10:26 +0200

    Final version

--- BirthdayParadox.py
+++ BirthdayParadox.py
@@ -1,19 +1,19 @@
 import datetime, random
 
-def getBirthdays(numberOfBirthdays):
+def get_birthdays(numb_of_birthdays):
     birthdays = []
-    for i in range(numberOfBirthdays):
-        startOfyear = datetime.date(2001, 1, 1)
+    for i in range(numb_of_birthdays):
+        start_of_year = datetime.date(2001, 1, 1)
         randomNumberOfDays = datetime.timedelta(random.randint(0, 364))
-        birthday = startOfyear + randomNumberOfDays
+        birthday = start_of_year + randomNumberOfDays
         birthdays.append(birthday)
     return birthdays
 
-def getMatch(birthdays):
+def get_match(birthdays):
     if len(birthdays) == len(set(birthdays)):
         return None
     for a, birthdayA in enumerate(birthdays):
-        for b, birthdayB in enumarate(birthdays[a+1:]):
+        for b, birthdayB in enumerate(birthdays[a+1:]):
             if birthdayA == birthdayB:
                 return birthdayA
 
@@ -29,4 +29,53 @@ while True:
 print()
 
 print("Here are", numbday, "birthdays:")
-birthdays = getBirthdays(numbday)
+birthdays = get_birthdays(numbday)
+
+for i, birthday in enumerate (birthdays):
+    if i != 0:
+        print(', ', end='')
+    monthName = MONTHS[birthday.month -1]
+    dateText = '{}{}'.format(monthName, birthday.day)
+    print(dateText, end='')
+print()
+print()
+
+match = get_match(birthdays)
+print('In this simulation ', end='')
+
+if match is not None:
+    monthName = MONTHS[match.month-1]
+    dateText = '{}{}'.format(monthName, match.day)
+    print('Multiple have a birthday on ', dateText)
+else:
+    print('There are no matching birthdays')
+print()
+
+print('Generating', numbday, 'random birthdays 100000 times')
+input('Press enter to begin')
+
+print('Let\'s run another 100000 simulations')
+
+simMatch = 0
+for i in range(100000):
+    if i % 10000 == 0:
+        print(i, 'simulations run...')
+    birthdays = get_birthdays(numbday)
+    if get_match(birthdays) is not None:
+        simMatch += 1
+print('100000 simulations run')
+
+probability = round(simMatch / 100000 * 100, 2)
+print('Out of 100000 simulations of', numbday, 'people, there was a')
+print('matching birthday in that group', simMatch, 'times. This means')
+print('that', numbday, 'people have a', probability, '% chance of')
+print('having a matching birthday in their group')
+print('That\'s probably more than you would think!')
+
+
+
+
+
+
+
+
